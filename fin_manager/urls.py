aa006359ed11ae21b2c74f5820126a84fe68353b
Joe Tal <5ptalemwajoel@gmail.com>
2026-03-15 13:08:35 +0300

    fix: Add all healthcheck endpoint variations for Leapcell

--- fin_manager/urls.py
+++ fin_manager/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path('expenses/', views.ExpenseListView.as_view(), name='expenses'),
     path('loans/', views.LoanListView.as_view(), name='loans'),
     path('healthz/', views.healthcheck, name='healthcheck'),
+    path('healthz', views.healthcheck, name='healthcheck_no_slash'),
     path('kaithhealthcheck/', views.healthcheck, name='kaithhealthcheck'),
+    path('kaithhealthcheck', views.healthcheck, name='kaithhealthcheck_no_slash'),
+    path('kaithheathcheck/', views.healthcheck, name='kaithheathcheck'),
+    path('kaithheathcheck', views.healthcheck, name='kaithheathcheck_no_slash'),
 ]
