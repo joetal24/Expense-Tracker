7271d13eb442e8a93a532487447e0bd5d3e10241
Joe Tal <5ptalemwajoel@gmail.com>
2026-03-15 13:43:09 +0300

    fix: Add custom security headers middleware

--- FinanceManager/settings.py
+++ FinanceManager/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'fin_manager.middleware.SecurityHeadersMiddleware',
 ]
 
 ROOT_URLCONF = 'FinanceManager.urls'
---
+++ fin_manager/middleware.py
@@ -0,0 +1,17 @@
+class SecurityHeadersMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        
+        # Security headers
+        response['X-Content-Type-Options'] = 'nosniff'
+        response['X-Frame-Options'] = 'DENY'
+        response['X-XSS-Protection'] = '1; mode=block'
+        response['Referrer-Policy'] = 'strict-origin-when-cross-origin'
+        
+        # Force HTTPS
+        response['Strict-Transport-Security'] = 'max-age=31536000; includeSubDomains; preload'
+        
+        return response
